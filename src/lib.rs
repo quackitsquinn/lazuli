57b7a032196a176821dc945247b40622206aa0a3
QuackitsQuinn <quinnweismiller@gmail.com>
2024-04-13 20:06:15 -0500

    migrate to a subfolder structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,4 @@
-#![allow(dead_code)] // TODO: Remove when codebase is more mature
-use std::{
-    any,
-    hash::{DefaultHasher, Hash, Hasher},
-};
+//! # rsocks: A TCP socket library oriented for game design
+//!
 
-mod header;
-mod sendable;
-
-/// Hashes the type_id of T.
-#[inline]
-fn hash_type_id<T: 'static>() -> u32 {
-    let mut hasher = DefaultHasher::new();
-    any::TypeId::of::<T>().hash(&mut hasher);
-    hasher.finish() as u32
-}
+pub use rsocks;
