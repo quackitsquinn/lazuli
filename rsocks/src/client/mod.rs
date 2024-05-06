c2f38b92c5772e3727906225ffe0350d50fc97f2
QuackitsQuinn <quinnweismiller@gmail.com>
2024-05-05 21:43:46 -0500

    Add a better make_client_server_pair implementation
    Remove the list of addresses and use a base address to make connections off of.

--- rsocks/src/client/mod.rs
+++ rsocks/src/client/mod.rs
@@ -9,30 +9,32 @@ pub use client::TcpClient;
 
 #[cfg(test)]
 mod test_utils {
-    use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+    use std::{
+        net::{IpAddr, Ipv4Addr, SocketAddr},
+        sync::{Mutex, OnceLock},
+    };
 
     use super::*;
-    static PORTS: [u16; 10] = [
-        13131, 13132, 13133, 13134, 13135, 13136, 13137, 13138, 13139, 13140,
-    ];
-    static ADDRESSES: [SocketAddr; 10] = [
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), PORTS[0]),
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), PORTS[1]),
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), PORTS[2]),
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), PORTS[3]),
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), PORTS[4]),
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), PORTS[5]),
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), PORTS[6]),
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), PORTS[7]),
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), PORTS[8]),
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), PORTS[9]),
-    ];
+    static PORTS_BASE: u16 = 5000;
+    static PORT_ACTIVE_BASE: Mutex<u16> = Mutex::new(PORTS_BASE);
 
     /// Creates a client and server pair.
     /// (client, server)
     pub(super) fn make_client_server_pair() -> (TcpClient, TcpClient) {
         use std::net::TcpListener;
-        let server = TcpListener::bind(ADDRESSES.as_slice()).expect("Unable to make socket");
+        let server = TcpListener::bind((Ipv4Addr::LOCALHOST, *PORT_ACTIVE_BASE.lock().unwrap()));
+        *PORT_ACTIVE_BASE.lock().unwrap() += 1;
+
+        if let Err(e) = server {
+            // If the port is in use, try again.
+            if e.kind() == std::io::ErrorKind::AddrInUse {
+                return make_client_server_pair();
+            } else {
+                panic!("Failed to bind server: {}", e);
+            }
+        }
+
+        let server = server.unwrap();
 
         let client = TcpClient::new(server.local_addr().unwrap()).unwrap();
         let server = server.accept().unwrap().0;
