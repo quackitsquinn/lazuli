b0dde407ca41fc4a3344724a798409cfb2fe36e5
QuackitsQuinn <quinnweismiller@gmail.com>
2024-05-18 22:57:15 -0500

    Simplify old code

--- lazuli_core/src/client/mod.rs
+++ lazuli_core/src/client/mod.rs
@@ -29,38 +29,16 @@ mod test_utils {
     /// (client, server)
     pub(super) fn make_client_server_pair() -> (Client, Client) {
         use std::net::TcpListener;
-        let server = TcpListener::bind((Ipv4Addr::LOCALHOST, 0));
-
-        if let Err(e) = server {
-            // If the port is in use, try again.
-            if e.kind() == std::io::ErrorKind::AddrInUse {
-                return make_client_server_pair();
-            } else {
-                panic!("Failed to bind server: {}", e);
-            }
-        }
-
-        let server = server.unwrap();
+        let server = TcpListener::bind((Ipv4Addr::LOCALHOST, 0)).expect("Failed to create server!");
 
         let client = Client::connect(server.local_addr().unwrap()).unwrap();
         let server = server.accept().unwrap().0;
 
         (client, Client::from_stream(server))
     }
-
+    /// Creates a Server. Expects the OS to assign a port.
     pub(super) fn make_server() -> Server {
-        let server = Server::new((Ipv4Addr::LOCALHOST, 0));
-
-        if let Err(e) = server {
-            // If the port is in use, try again.
-            if e.kind() == std::io::ErrorKind::AddrInUse {
-                return make_server();
-            } else {
-                panic!("Failed to bind server: {}", e);
-            }
-        }
-
-        server.unwrap()
+        Server::new((Ipv4Addr::LOCALHOST, 0)).expect("Failed to create server!")
     }
 
     /// Tests sending and receiving data. Convenience function for testing.
